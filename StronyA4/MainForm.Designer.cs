fdf0ac733e0f687fd5cbe6e9b30e2f36888f57c1
Damian Hintz <damian.hintz@gmail.com>
2017-01-19 11:28:30 +0100

    Dodaj nowy projekt StronyA4

---
+++ StronyA4/MainForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace StronyA4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.status = new System.Windows.Forms.StatusStrip();
+            this.plikMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomocMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel = new System.Windows.Forms.Panel();
+            this.folderView = new System.Windows.Forms.ListView();
+            this.folderHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menu.SuspendLayout();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.BackColor = System.Drawing.SystemColors.Menu;
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikMenuItem,
+            this.pomocMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menu.Size = new System.Drawing.Size(910, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // status
+            // 
+            this.status.BackColor = System.Drawing.SystemColors.Menu;
+            this.status.Location = new System.Drawing.Point(0, 410);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(910, 22);
+            this.status.TabIndex = 1;
+            this.status.Text = "statusStrip1";
+            // 
+            // plikMenuItem
+            // 
+            this.plikMenuItem.Name = "plikMenuItem";
+            this.plikMenuItem.Size = new System.Drawing.Size(34, 20);
+            this.plikMenuItem.Text = "Plik";
+            // 
+            // pomocMenuItem
+            // 
+            this.pomocMenuItem.Name = "pomocMenuItem";
+            this.pomocMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.pomocMenuItem.Text = "Pomoc";
+            // 
+            // folderMenu
+            // 
+            this.folderMenu.BackColor = System.Drawing.SystemColors.Menu;
+            this.folderMenu.Name = "folderMenu";
+            this.folderMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.folderMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.folderView);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel.Location = new System.Drawing.Point(0, 24);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(910, 386);
+            this.panel.TabIndex = 3;
+            // 
+            // folderView
+            // 
+            this.folderView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.folderHeader});
+            this.folderView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.folderView.Location = new System.Drawing.Point(0, 0);
+            this.folderView.Name = "folderView";
+            this.folderView.Size = new System.Drawing.Size(910, 386);
+            this.folderView.TabIndex = 0;
+            this.folderView.UseCompatibleStateImageBehavior = false;
+            this.folderView.View = System.Windows.Forms.View.Details;
+            // 
+            // folderHeader
+            // 
+            this.folderHeader.Text = "Folder";
+            this.folderHeader.Width = 400;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 432);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "MainForm";
+            this.Text = "StronyA4 v1.0-beta - Zliczaj strony A4 w plikach (19 stycznia 2017)";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem plikMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pomocMenuItem;
+        private System.Windows.Forms.StatusStrip status;
+        private System.Windows.Forms.ContextMenuStrip folderMenu;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.ListView folderView;
+        private System.Windows.Forms.ColumnHeader folderHeader;
+    }
+}
+
